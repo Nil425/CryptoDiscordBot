19be02e8411694996159ed74c91b72016ff4cfbe
Nil425 <87794772+Nil425@users.noreply.github.com>
2022-06-09 17:45:45 -0400

    Update TA_Bot.py

--- TA_Bot.py
+++ TA_Bot.py
@@ -53,7 +53,7 @@ def retrieve_data(arg1):
     return rest_api.get_bars(arg1, TimeFrame.Day, dstr, d2str).df
 
 
-#METHODS GENERRATING THE VARIOUS TECHNICAL PLOTS
+#METHODS GENERATING THE VARIOUS TECHNICAL PLOTS
 
 def gen_moving_averages(arg1):
     stock_bars = retrieve_data(arg1)
@@ -65,10 +65,10 @@ def gen_moving_averages(arg1):
     df_hma20 = stock_bars.ta.hma(length=20)
     df_hma50 = stock_bars.ta.hma(length=50)
 
-    df_bbL20 = stock_bars.ta.accbands(length=20)
+    
 
     fig, ax = plt.subplots()
-    plt.title(arg1.upper())
+    plt.title(arg1.upper() + " Moving Averages")
 
     ax.plot(stock_bars["close"], '-k')
     ax.plot(df_sma20, 'lightsteelblue', label="sma20")
@@ -76,47 +76,80 @@ def gen_moving_averages(arg1):
     ax.plot(df_sma80, 'violet', label="sma80")
     ax.plot(df_hma20, 'wheat', label="hma20")
     ax.plot(df_hma50, 'lightcoral', label="hma50")
-    ax.plot(df_bbL20, 'rosybrown', label="BB20")
     ax.legend(loc="upper left")
 
     plt.savefig("output.png")
 
+def gen_volatility(arg1):
+  stock_bars = retrieve_data(arg1)
+  
+  df_acc50 = stock_bars.ta.accbands()
+  df_rvi = stock_bars.ta.rvi()
 
+  fig, dx = plt.subplots()
+  title = arg1.upper() + " acc bands"
+  plt.title(title)
+  dx.plot(df_acc50, 'rosybrown', label="acc50")
+  dx.plot(stock_bars["close"], '-k')
+  dx.legend(loc="upper left")
+  plt.savefig("output4.png")
+
+  fig, ex = plt.subplots()
+  title = arg1.upper() + " relative volatility index"
+  plt.title(title)
+  ex.plot(df_rvi, 'violet', label="rvi50")
+  ex.legend(loc="upper left")
+  plt.savefig("output5.png")
+  
+  
 def gen_momentum(arg1):
     stock_bars = retrieve_data(arg1)
 
     df_macd = stock_bars.ta.macd()
-
+    df_rsi = stock_bars.ta.rsi()
+    
     fig, bx = plt.subplots()
     title = arg1 + ' Momentum'
     plt.title(title)
     bx.plot(df_macd, 'aquamarine', label="macd")
     bx.legend(loc="upper left")
-
     plt.savefig("output2.png")
-
-
-def gen_rsi(arg1):
-    stock_bars = retrieve_data(arg1)
-
-    df_rsi = stock_bars.ta.rsi()
 
     fig, cx = plt.subplots()
     title2 = arg1 + ' Relative Strength'
     plt.title(title2)
     cx.plot(df_rsi, 'salmon', label="RSI")
     cx.legend(loc="upper left")
+    plt.savefig("output3.png")
 
+def gen_trend(arg1):
+    stock_bars = retrieve_data(arg1)
+
+    df_vortex = stock_bars.ta.vortex()
+    df_adx = stock_bars.ta.adx()
+    
+    fig, bx = plt.subplots()
+    title = arg1 + ' vortex'
+    plt.title(title)
+    bx.plot(df_vortex, 'aquamarine', label="vortex")
+    bx.legend(loc="upper left")
+    plt.savefig("output2.png")
+
+    fig, cx = plt.subplots()
+    title2 = arg1 + ' adx'
+    plt.title(title2)
+    cx.plot(df_adx, 'salmon', label="adx")
+    cx.legend(loc="upper left")
     plt.savefig("output3.png")
 
 
-#COMMANDS TO INVOKE VARIOUS TECHNICAL PLOTS
+#COMMANDS INVOKING THE VARIOUS TECHNICAL PLOTS
 
 
 @client.command()
 async def show_moving_averages(ctx, ticker):
     gen_moving_averages(ticker)
-    #await bot.send_file(ctx.message.channel, f['output.png'], f['output2.png'], f['output3.png'])
+    #await ctx.send_file(ctx.message.channel, f['output.png'], f['output2.png'], f['output3.png'])
     await ctx.send(file=discord.File('output.png'))
     #await ctx.send(file=discord.File['output.png'], file1=discord.File['output2.png'], file2=discord.File['output3.png'])
 
@@ -125,9 +158,18 @@ async def show_moving_averages(ctx, ticker):
 async def show_momentum(ctx, ticker):
     gen_momentum(ticker)
     await ctx.send(file=discord.File('output2.png'))
-
+    await ctx.send(file=discord.File('output3.png'))
+    #await ctx.send(file=discord.File['output3.png'])
 
 @client.command()
-async def show_rsi(ctx, ticker):
-    gen_rsi(ticker)
+async def show_volatility(ctx, ticker):
+    gen_volatility(ticker)
+    await ctx.send(file=discord.File('output4.png'))
+    await ctx.send(file=discord.File('output5.png'))
+
+client.command()
+async def show_trend(ctx, ticker):
+    gen_trend(ticker)
+    await ctx.send(file=discord.File('output2.png'))
     await ctx.send(file=discord.File('output3.png'))
+    #await ctx.send(file=discord.File['output3.png'])
